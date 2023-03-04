01a1c358d699b9fb864bc10ca6989aff74bb2ff5
Nino Lucillo <nino@grafikbuam.de>
2023-03-04 15:24:17 +0100

    update repo 2/2

--- craft/config/app.php
+++ craft/config/app.php
@@ -22,9 +22,9 @@ use craft\helpers\App;
 return [
     'id' => App::env('APP_ID') ?: 'CraftCMS',
     'modules' => [
-        'my-module' => \modules\Module::class,
-        'mamp-helper' => \modules\MampHelper::class,
+        //'my-module' => \modules\Module::class,
+       // 'mamp-helper' => \modules\MampHelper::class,
      ],
-     'bootstrap' => ['mamp-helper'],
+     //'bootstrap' => ['mamp-helper'],
     //'bootstrap' => ['my-module'],
 ];
--- craft/config/feed-me.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    '*' => [
-        'pluginName' => 'EliteProspectsAPI',
-        'parseTwig' => true,
-    ]
-];
