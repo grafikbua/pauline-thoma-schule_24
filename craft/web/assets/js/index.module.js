844264f13f49247d3a4a3551f0fbe0f4328afd2c
grafikbua <pedro@grafikbuam.de>
2025-02-19 11:16:44 +0100

    bugfixes and updates

--- craft/web/assets/js/index.module.js
+++ craft/web/assets/js/index.module.js
@@ -10,5 +10,65 @@
 "use strict";
 
 require("../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js");
+// Polyfills
+// import './polyfills/custom-event';
+
+// Node Modules
+
+// import Lazyload from 'vanilla-lazyload';
+// import Rellax from 'rellax';
+// import { tns } from 'tiny-slider';
+// import "../../node_modules/fslightbox/index.js";
+// import "../../node_modules/masonry-layout/masonry.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  var burger = document.querySelector(".burger");
+  var overlay = document.querySelector(".overlay");
+  var mobileNav = document.querySelector(".mobile-nav");
+  var body = document.body;
+  function toggleMenu() {
+    burger.classList.toggle("clicked");
+    overlay.classList.toggle("show");
+    mobileNav.classList.toggle("show");
+    body.classList.toggle("overflow");
+  }
+  burger.addEventListener("click", toggleMenu);
+  overlay.addEventListener("click", toggleMenu);
+});
+window.onscroll = function () {
+  scrollFunction();
+};
+var header = document.getElementById("js-header");
+var logo = document.getElementById("js-logo");
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    logo.style.height = "80px";
+    header.classList.remove("shadow-sm");
+    header.classList.add("shadow");
+  } else {
+    logo.style.height = "130px";
+    header.classList.add("shadow-sm");
+    header.classList.remove("shadow");
+  }
+}
+;
+(function () {
+  var check = document.createElement('script');
+  if (!('noModule' in check) && 'onbeforeload' in check) {
+    var support = false;
+    document.addEventListener('beforeload', function (e) {
+      if (e.target === check) {
+        support = true;
+      } else if (!e.target.hasAttribute('nomodule') || !support) {
+        return;
+      }
+      e.preventDefault();
+    }, true);
+    check.type = 'module';
+    check.src = '.';
+    document.head.appendChild(check);
+    check.remove();
+  }
+})();
 
 },{"../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js":1}]},{},[2]);
--- src/js/index.es6.js
+++ src/js/index.es6.js
@@ -11,3 +11,66 @@ import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    const burger = document.querySelector(".burger");
+    const overlay = document.querySelector(".overlay");
+    const mobileNav = document.querySelector(".mobile-nav");
+    const body = document.body;
+
+    function toggleMenu() {
+    burger.classList.toggle("clicked");
+    overlay.classList.toggle("show");
+    mobileNav.classList.toggle("show");
+    body.classList.toggle("overflow");
+    }
+
+    burger.addEventListener("click", toggleMenu);
+    overlay.addEventListener("click", toggleMenu);
+});
+    
+    
+    
+
+window.onscroll = function() {scrollFunction()};
+var header = document.getElementById("js-header");
+var logo = document.getElementById("js-logo")
+
+function scrollFunction() {
+if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    logo.style.height = ("80px");
+    header.classList.remove("shadow-sm");
+    header.classList.add("shadow");
+} else {
+    logo.style.height = ("130px");
+    header.classList.add("shadow-sm");
+    header.classList.remove("shadow");
+}
+};
+
+    
+    
+    
+
+(function () {
+var check = document.createElement('script');
+if (!('noModule' in check) && 'onbeforeload' in check) {
+var support = false;
+document.addEventListener('beforeload', function (e) {
+if (e.target === check) {
+support = true;
+} else if (! e.target.hasAttribute('nomodule') || ! support) {
+return;
+}
+e.preventDefault();
+}, true);
+
+check.type = 'module';
+check.src = '.';
+document.head.appendChild(check);
+check.remove();
+}
+}());
+
+    
+
+    
